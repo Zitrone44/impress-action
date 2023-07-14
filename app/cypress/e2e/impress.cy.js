9248e5fa3041babb2164a34157ac30105b797460
Jonas-Ian Kuche <jonas-ian.kuche@mni.thm.de>
2023-07-14 10:59:32 +0200

    feat: add end detection

--- app/cypress/e2e/impress.cy.js
+++ app/cypress/e2e/impress.cy.js
@@ -1,15 +1,24 @@
+function recursiveContinue(startHash, i) {
+    cy.screenshot(`screenshot-${i.toString().padStart(4, '0')}.png`, { capture: 'viewport' })
+    cy.get('body').trigger('keydown', { keyCode: 39, force: true });
+    cy.wait(500);
+    cy.get('body').trigger('keyup', { keyCode: 39, force: true });
+    cy.wait(2000);
+    cy.hash().then((currentHash) => {
+        if (startHash !== currentHash) {
+            recursiveContinue(startHash, i+1);
+        }
+    })
+}
+
 describe('template spec', () => {
-  it('passes', () => {
-    console.log('ENV', Cypress.env())
-//    cy.viewport(2999, 2999)
-    cy.viewport(1920, 1080)
-    cy.visit(Cypress.env('PRESENTATION_URL'))
-    for (let i = 0; i < 10; i++) {
-        cy.screenshot({ capture: 'viewport' })
-        cy.get('body').trigger('keydown', { keyCode: 39, force: true });
-        cy.wait(500);
-        cy.get('body').trigger('keyup', { keyCode: 39, force: true });
-        cy.wait(2000);
-    }
-  })
-})
+    it('passes', () => {
+        console.log('ENV', Cypress.env())
+        cy.viewport(1800, 1000)
+        cy.visit(Cypress.env('PRESENTATION_URL'))
+        cy.wait(5000);
+        cy.hash().then((startHash) => {
+            recursiveContinue(startHash, 0)
+        })
+    });
+});
